ac6b8e0ff6323568f0a876c63f1673edb6332e66
MeshokQW <grinya.pan24@gmail.com>
2025-10-17 22:01:45 +0300

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,70 @@
+from random import randint
+import requests
+
+class Pokemon:
+    pokemons = {}
+    # Инициализация объекта (конструктор)
+    def __init__(self, pokemon_trainer):
+        self.pokemon_trainer = pokemon_trainer   
+        self.pokemon_number = randint(1,1000)
+        self.hp = randint(75, 125)
+        self.power = randint(10, 25)
+
+        #delta = (100 - self.hp) // 2
+        #self.power = randint(10, 30) + delta
+
+        self.img = self.get_img()
+        self.name = self.get_name()
+
+        Pokemon.pokemons[pokemon_trainer] = self
+
+    # Метод для получения картинки покемона через API
+    def get_img(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']['front_default'])
+        else:
+            return "Pikachu"
+    
+    # Метод для получения имени покемона через API
+    def get_name(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['forms'][0]['name'])
+        else:
+            return "Pikachu"
+
+
+    # Метод класса для получения информации
+    def info(self):
+        return f"Имя твоего покемона: {self.name} hp: {self.hp} power: {self.power}"
+
+    # Метод класса для получения картинки покемона
+    def show_img(self):
+        return self.img
+    
+    def attack(self, enemy):
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer} осталось {enemy.hp}"
+        else:
+            enemy.hp = 0
+            Pokemon.loosers.append(self.pokemon_trainer)
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+
+class Fighter(Pokemon):
+
+    def __init__(self, pokemon_trainer):
+        super().__init__(pokemon_trainer)
+        self.power += randint(30, 50)
+
+class Tank(Pokemon):
+
+    def __init__(self, pokemon_trainer):
+        super().__init__(pokemon_trainer)
+        self.hp += randint(90, 150)
+
